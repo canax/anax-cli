7a9dc8d1527e49e553883cce8bf1c8b8bb61e48d
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-08-04 14:28:27 +0200

    adding selfupdate and base for configuration

--- REVISION.md
+++ REVISION.md
@@ -1,6 +1,14 @@
 Revision history
 =================================
 
+v1.0.2* (2017-08-04)
+---------------------------------
+
+* Made installation procedure work.
+* Adding selfupdate.
+* Adding configdirectory.
+
+
 v1.0.1 (2017-07-15)
 ---------------------------------
 
--- src/anax.bash
+++ src/anax.bash
@@ -6,7 +6,7 @@
 #
 # Globals (prefer none)
 # 
-
+readonly ANAX_CONFIG_DIR="$HOME/.anax"
 
 
 #
@@ -14,7 +14,7 @@
 #
 version()
 {
-    printf "v1.0.1 (2017-07-15)\\n"
+    printf "v1.0.2* (2017-08-04)\\n"
 }
 
 
@@ -29,11 +29,14 @@ Utility to work with Anax web sites.
 Usage: anax [options] <command> [arguments]
 
 Command:
-  check [url]           Check all links starting from url.
+ config                   Create base for configuration in \$HOME/.anax/.
+ create <dir> <template>  Create a new site in dir using a template.
+ selfupdate               Update to latest version.
 
 Options:
-    --help, -h          Show info on how to use it.
-    --output [format]   Set the output format, default is text.
+ --help, -h          Show info on how to use it.
+ --version, -v       Show info on how to use it.
+ --force, -f         Force operation even though it should not.
 "
 }
 
@@ -72,18 +75,66 @@ fail()
 
 
 #
-# Create a new site
+# Read the configuration if it exists
+#
+config_read()
+{
+    local configFile="$ANAX_CONFIG_DIR/config"
+
+    # shellcheck source=/dev/null
+    [[ -f $configFile ]] && . "$configFile"
+}
+
+
+
+#
+# Create the configuration
+#
+config_create()
+{
+    install -d "$ANAX_CONFIG_DIR/scaffold" || fail "Could not create configuration dir: '$ANAX_CONFIG_DIR'"
+    touch "$ANAX_CONFIG_DIR/config" || fail "Could not create configuration file: '$ANAX_CONFIG_DIR/config'"
+    ls -l "$ANAX_CONFIG_DIR"
+}
+
+
+
+#
+# Create the configuration directory.
+#
+anax_config()
+{
+    config_create
+}
+
+
+
+#
+# Create (scaffold) a new site by using a template
 #
 anax_create()
 {
-    local dir=$ARGS
+    local dir=${ARGS[0]}
+    local template=${ARGS[1]}
+
+    config_read
 
     [[ ! $dir ]] && fail "Missing name of directory to create the site in, must be non-existing directory."
 
-    [ -e "$dir" ] && fail "The directory exists, use another path where a new directory can be created."
+    [[ -e "$dir" && ! $FORCE ]] && fail "The directory '$dir' exists, use another dirname."
 
-    echo "Creating a new Anax site in directory '$dir'."
+    echo "Creating a new Anax site in directory '$dir' using template '$template'."
     install -d "$dir" || echo "failed"
+}
+
+
+
+#
+# Selfupdate to latest version.
+#
+anax_selfupdate()
+{
+    curl --silent https://raw.githubusercontent.com/canax/anax-cli/master/src/install.bash | bash
 }
 
 
@@ -106,7 +157,14 @@ main()
                 exit 0
             ;;
 
-            create)
+            --force | -f)
+                FORCE=1
+                shift
+            ;;
+
+            create      | \
+            config      | \
+            selfupdate    )
                 COMMAND=$1
                 shift
             ;;
