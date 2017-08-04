9a6291e40d0474c2db7ba4404ca034a8368f4075
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-08-04 14:20:25 +0200

    Made installation procedure work.

--- src/install.bash
+++ src/install.bash
@@ -12,18 +12,18 @@
 #
 # Basic settings
 #
-TARGET="https://raw.githubusercontent.com/mosbth/dbwebb-cli/master/dbwebb2"
+TARGET="https://raw.githubusercontent.com/canax/anax-cli/master/src/anax.bash"
 PATH1="/usr/local/bin"
 PATH2="/usr/bin"
-WHERE="$PATH1/dbwebb"
+WHERE="$PATH1/anax"
 TMP="/tmp/$$"
 
 ECHO="echo -e"
 ECHON="echo -n"
-MSG_DOING="\\033[1;37;40m[dbwebb-cli install]\\033[0m"
+MSG_DOING="\\033[1;37;40m[anax-cli]\\033[0m"
 #MSG_DONE="\033[0;30;42m[OK]\033[0m"
 MSG_DONE=""
-MSG_OK="\\033[0;30;42m[SUCCESS]\\033[0m"
+#MSG_OK="\\033[0;30;42m[SUCCESS]\\033[0m"
 #MSG_WARNING="\\033[43mWARNING\\033[0m"
 MSG_FAILED="\\033[0;37;41m[FAILED]\\033[0m"
 
@@ -42,10 +42,10 @@ function checkTool() {
 
 $ECHO "$MSG_DOING Checking precondition..."
 
-checkTool "wget"   "using your packet manager."
-checkTool "ssh"    "using your packet manager."
-checkTool "rsync"  "using your packet manager."
-checkTool "git"    "https://dbwebb.se/labbmiljo/git"
+checkTool "git"       "https://dbwebb.se/labbmiljo/git"
+checkTool "php"       "https://dbwebb.se/labbmiljo/php-i-pathen"
+checkTool "composer"  "https://dbwebb.se/labbmiljo/composer"
+checkTool "make"      "https://dbwebb.se/labbmiljo/composer"
 
 $ECHO "\\n$MSG_DONE"
 
@@ -54,13 +54,13 @@ $ECHO "\\n$MSG_DONE"
 #
 # Download
 #
-$ECHO "$MSG_DOING Downloading dbwebb-cli..."
+$ECHO "$MSG_DOING Downloading anax-cli..."
 
-if wget -qO $TMP $TARGET; then
+if ! wget -qO $TMP $TARGET; then
     rm -f $TMP
-    $ECHO "$MSG_FAILED downloading dbwebb-cli."
+    $ECHO "$MSG_FAILED downloading anax-cli."
     $ECHO "I could not download the script from GitHub."
-    $ECHO "Do you have a network connection to GitHub http://github.com?"
+    $ECHO "Failed to access: $TARGET"
     exit 1
 fi
 
@@ -71,22 +71,20 @@ $ECHO "$MSG_DONE"
 
 
 #
-# Installing dbwebb-cli into path
+# Installing into path
 #
-$ECHO "$MSG_DOING Installing dbwebb-cli..."
+$ECHO "$MSG_DOING Installing anax-cli..."
 
-if [ ! -d "$PATH1" ]; then
+if [[ ! -d $PATH1 ]]; then
     WHERE="$PATH2/dbwebb"
 fi
 
 $ECHO "Installing into '$WHERE'."
 
-if install -v -m 0755 $TMP $WHERE; then
+if ! install -v -m 0755 $TMP $WHERE; then
     rm $TMP
     $ECHO "$MSG_FAILED installing into '$WHERE'."
     $ECHO "Try re-run the installation-command as root using 'sudo'."
-    $ECHO "Or, install using the manual procedure, as explained here:"
-    $ECHO " https://dbwebb.se/dbwebb-cli#steg"
     exit 1
 fi
 
@@ -112,8 +110,8 @@ $ECHO "$MSG_DONE"
 #
 $ECHO "$MSG_DOING Check what version we have..."
 
-if dbwebb --version; then
-    $ECHO "$MSG_FAILED checking the version of dbwebb-cli."
+if ! anax --version; then
+    $ECHO "$MSG_FAILED checking the version of anax-cli."
     $ECHO "Try re-running the installation script or post the output of the installation procedure to the forum and ask for help."
     exit 1
 fi
@@ -126,6 +124,5 @@ $ECHO "$MSG_DONE"
 # Done
 #
 $ECHO "$MSG_DOING Done with success!"
-$ECHO "$MSG_OK"
-$ECHO "Execute 'dbwebb --help' to get an overview of the command."
-$ECHO "Read the manual: https://dbwebb.se/dbwebb-cli"
+$ECHO "Execute 'anax --help' to get an overview of the command."
+$ECHO "Read the manual: https://dbwebb.se/anax-cli"
